e77ead16afd17d02425dbc292d3319ea4a2bb40b
ngatchou <ngatchou@aspi>
2025-07-05 21:12:14 +0100

    updated link redirect

--- health_app/urls.py
+++ health_app/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
+
+    path('', views.CustomLoginView.as_view(), name='home'),
     # Registration & Auth
     path('register/', views.HospitalRegistrationView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
